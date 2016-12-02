aecc3f39be5ca52216554b9d602b97a302aa6767
Rouven Spreckels <n3vu0r@qu1x.org>
2016-12-02 22:57:52 +0100

    Fixed license header.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //
 // Signifix is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License version 3
-// as published by the Free Software Foundation on 19 November 2007.
+// as published by the Free Software Foundation on 29 June 2007.
 //
 // Signifix is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
