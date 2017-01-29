cd8767be14840fdcbcceea79e26db068dd2fa373
Rouven Spreckels <n3vu0r@qu1x.org>
2017-01-29 16:25:06 +0100

    Shrank Signifix from 24 to 4 B. Fields -> Methods.

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@
 //!
 //! Signifix guarantees a fixed number of significant figures and a fixed number
 //! of resulting characters by automatically choosing the metric unit prefix and
-//! appropriately adjusting the floating point precision.
+//! appropriately adjusting the decimal point position.
 //!
 //! # Usage
 //!
@@ -48,6 +48,7 @@
 //!
 //! ```
 //! # #![feature(try_from)]
+//! #
 //! use std::convert::TryFrom; // Until stabilized.
 //!
 //! use signifix::{Signifix, Result};
@@ -73,6 +74,7 @@
 //!
 //! ```
 //! # #![feature(try_from)]
+//! #
 //! use std::convert::TryFrom; // Until stabilized.
 //!
 //! use signifix::{Signifix, Result};
@@ -82,8 +84,8 @@
 //! };
 //!
 //! assert_eq!(format_rate(42_667, 300).ok(), Some("142.2  B/s".into()));
-//! assert_eq!(format_rate(42_667,  30).ok(), Some("1.422 kB/s".into()));
-//! assert_eq!(format_rate(42_667,   3).ok(), Some("14.22 kB/s".into()));
+//! assert_eq!(format_rate(42_667, 030).ok(), Some("1.422 kB/s".into()));
+//! assert_eq!(format_rate(42_667, 003).ok(), Some("14.22 kB/s".into()));
 //! ```
 //!
 //! Or to monitor a measured quantity like an electrical current including its
@@ -91,6 +93,7 @@
 //!
 //! ```
 //! # #![feature(try_from)]
+//! #
 //! use std::convert::TryFrom; // Until stabilized.
 //!
 //! use signifix::{Signifix, Result};
@@ -108,6 +111,7 @@
 //!
 //! ```
 //! # #![feature(try_from)]
+//! #
 //! use std::convert::TryFrom; // Until stabilized.
 //!
 //! use signifix::{Signifix, Result};
@@ -120,10 +124,18 @@
 //! assert_eq!(format_diff(27_473, 46_839).ok(), Some("-19.37 kB".into()));
 //! ```
 
-#![feature(field_init_shorthand)]
 #![feature(try_from)]
 
 use std::convert::TryFrom;
+
+use std::result;
+use std::error;
+
+use std::fmt;
+use fmt::{Display, Formatter};
+
+use std::cmp::Ordering;
+use Ordering::Equal;
 
 /// An error arising from this crate's `TryFrom` trait implementation for its
 /// `Signifix` type.
@@ -132,30 +144,34 @@ pub enum Error {
 	/// The given number is below the lower bound ±1.000 y (= ±1e-24) of the
 	/// lowermost metric unit prefix yocto (y = 1e-24).
 	OutOfLowerBound(f64),
+
 	/// The given number is above the upper bound ±999.9 Y (≅ ±1e+27) of the
 	/// uppermost metric unit prefix yotta (Y = 1e+24).
 	OutOfUpperBound(f64),
+
 	/// The given number is actually Not a Number (NaN).
 	Nan,
 }
 
 impl Eq for Error {}
 
-impl std::fmt::Display for Error {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		use std::error::Error;
+impl Display for Error {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
 		match *self {
-			::Error::OutOfLowerBound(number) =>
-				write!(f, "{} for number {:+.3e}", self.description(), number),
-			::Error::OutOfUpperBound(number) =>
-				write!(f, "{} for number {:+.3e}", self.description(), number),
-			::Error::Nan =>
-				write!(f, "{}", self.description()),
+			Error::OutOfLowerBound(number) =>
+				write!(f, "{} for number {:+.3e}",
+					error::Error::description(self), number),
+			Error::OutOfUpperBound(number) =>
+				write!(f, "{} for number {:+.3e}",
+					error::Error::description(self), number),
+			Error::Nan =>
+				write!(f, "{}",
+					error::Error::description(self)),
 		}
 	}
 }
 
-impl std::error::Error for Error {
+impl error::Error for Error {
 	fn description(&self) -> &str {
 		match *self {
 			Error::OutOfLowerBound(..) =>
@@ -166,7 +182,7 @@ impl std::error::Error for Error {
 				"Not a Number (NaN)",
 		}
 	}
-	fn cause(&self) -> Option<&std::error::Error> {
+	fn cause(&self) -> Option<&error::Error> {
 		match *self {
 			Error::OutOfLowerBound(..) => None,
 			Error::OutOfUpperBound(..) => None,
@@ -176,12 +192,12 @@ impl std::error::Error for Error {
 }
 
 /// The canonical `Result` type using this crate's `Error` type.
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = result::Result<T, Error>;
 
 /// Intermediate implementor type of this crate's `TryFrom` and `Display` trait
 /// implementations. Former tries to convert a number to this type by
 /// automatically choosing the metric unit prefix and appropriately adjusting
-/// the floating point precision while latter uses this type's fields to format
+/// the decimal point position while latter uses this type's fields to format
 /// the number to a string of a fixed number of significant figures and a fixed
 /// number of resulting characters. The format parameter `{}` prefixes negative
 /// numbers with a minus sign. The variant `{:+}` additionally prefixes positive
@@ -189,29 +205,28 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// to align with negative numbers.
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Signifix {
-	/// Automatically chosen metric unit prefix index ranging from 0 to 16.
-	pub prefix: usize,
-	/// Appropriately adjusted floating point precision ranging from 1 to 3.
-	pub precision: usize,
-	/// Metrically normalized signed significand ranging from ±1.000 to ±999.9.
-	pub significand: f64,
+	numerator: i16,
+	exponent: u8,
+	prefix: u8,
 }
 
 impl Eq for Signifix {}
 
 impl Ord for Signifix {
-	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-		let ordering = self.prefix.cmp(&other.prefix);
-		if ordering == std::cmp::Ordering::Equal {
-			self.significand.partial_cmp(&other.significand).unwrap()
-		} else {
-			ordering
+	fn cmp(&self, other: &Self) -> Ordering {
+		let mut ordering = self.prefix.cmp(&other.prefix);
+		if ordering == Equal {
+			ordering = other.exponent.cmp(&self.exponent);
+			if ordering == Equal {
+				ordering = self.numerator.cmp(&other.numerator)
+			}
 		}
+		ordering
 	}
 }
 
 impl PartialOrd for Signifix {
-	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
 		Some(self.cmp(other))
 	}
 }
@@ -222,13 +237,6 @@ pub const MIN_LEN: usize = 7;
 /// Number of resulting characters when a sign or whitespace is prefixed.
 pub const MAX_LEN: usize = 8;
 
-/// Metric unit prefix factors from 1e-24 to 1e+24 indexed from 0 to 16.
-pub const FACTOR: [f64; 17] = [
-	1e-24, 1e-21, 1e-18, 1e-15, 1e-12, 1e-09, 1e-06, 1e-03,
-	1e+00,
-	1e+03, 1e+06, 1e+09, 1e+12, 1e+15, 1e+18, 1e+21, 1e+24,
-];
-
 /// Metric unit prefix symbols from y to Y indexed from 0 to 16.
 pub const SYMBOL: [char; 17] = [
 	'y', 'z', 'a', 'f', 'p', 'n', 'µ', 'm',
@@ -236,88 +244,166 @@ pub const SYMBOL: [char; 17] = [
 	'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y',
 ];
 
+/// Metric unit prefix factors from 1e-24 to 1e+24 indexed from 0 to 16.
+pub const FACTOR: [f64; 17] = [
+	1e-24, 1e-21, 1e-18, 1e-15, 1e-12, 1e-09, 1e-06, 1e-03,
+	1e+00,
+	1e+03, 1e+06, 1e+09, 1e+12, 1e+15, 1e+18, 1e+21, 1e+24,
+];
+
+impl Signifix {
+	/// Metrically normalized signed significand from ±1.000 to ±999.9.
+	pub fn significand(&self) -> f64 {
+		self.numerator() as f64 / self.denominator() as f64
+	}
+
+	/// Signed significand numerator from ±1,000 to ±9,999.
+	pub fn numerator(&self) -> i32 {
+		self.numerator.into()
+	}
+
+	/// Significand denominator of either 10, 100, or 1,000.
+	pub fn denominator(&self) -> i32 {
+		[1, 10, 100, 1_000][self.exponent()]
+	}
+
+	/// Exponent of significand denominator of either 1, 2, or 3.
+	pub fn exponent(&self) -> usize {
+		self.exponent.into()
+	}
+
+	/// Metric unit prefix as `SYMBOL` and `FACTOR` array index from 0 to 16.
+	pub fn prefix(&self) -> usize {
+		self.prefix.into()
+	}
+
+	/// Symbol of metric unit prefix from y to Y.
+	pub fn symbol(&self) -> char {
+		SYMBOL[self.prefix()]
+	}
+
+	/// Factor of metric unit prefix from 1e-24 to 1e+24.
+	pub fn factor(&self) -> f64 {
+		FACTOR[self.prefix()]
+	}
+}
+
 impl TryFrom<i8> for Signifix {
 	type Err = Error;
-	fn try_from(number: i8) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: i8) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<i16> for Signifix {
 	type Err = Error;
-	fn try_from(number: i16) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: i16) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<i32> for Signifix {
 	type Err = Error;
-	fn try_from(number: i32) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: i32) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<i64> for Signifix {
 	type Err = Error;
-	fn try_from(number: i64) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: i64) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<u8> for Signifix {
 	type Err = Error;
-	fn try_from(number: u8) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: u8) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<u16> for Signifix {
 	type Err = Error;
-	fn try_from(number: u16) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: u16) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<u32> for Signifix {
 	type Err = Error;
-	fn try_from(number: u32) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: u32) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<u64> for Signifix {
 	type Err = Error;
-	fn try_from(number: u64) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: u64) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<isize> for Signifix {
 	type Err = Error;
-	fn try_from(number: isize) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: isize) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<usize> for Signifix {
 	type Err = Error;
-	fn try_from(number: usize) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: usize) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<f32> for Signifix {
 	type Err = Error;
-	fn try_from(number: f32) -> Result<Self> { Self::try_from(number as f64) }
+
+	fn try_from(number: f32) -> Result<Self> {
+		Self::try_from(number as f64)
+	}
 }
 
 impl TryFrom<f64> for Signifix {
 	type Err = Error;
+
 	fn try_from(number: f64) -> Result<Self> {
-		let (significand, prefix) = {
+		let (numerator, prefix) = {
 			let number = number.abs();
-			if number < FACTOR[8] {
-				let prefix = if number < FACTOR[4] {
-					if number < FACTOR[2] {
-						if number < FACTOR[1] { 0 } else { 1 }
+			if number < FACTOR[08] {
+				let prefix = if number < FACTOR[04] {
+					if number < FACTOR[02] {
+						if number < FACTOR[01] { 00 } else { 01 }
 					} else {
-						if number < FACTOR[3] { 2 } else { 3 }
+						if number < FACTOR[03] { 02 } else { 03 }
 					}
 				} else {
-					if number < FACTOR[6] {
-						if number < FACTOR[5] { 4 } else { 5 }
+					if number < FACTOR[06] {
+						if number < FACTOR[05] { 04 } else { 05 }
 					} else {
-						if number < FACTOR[7] { 6 } else { 7 }
+						if number < FACTOR[07] { 06 } else { 07 }
 					}
 				};
 				(number / FACTOR[prefix], prefix)
 			} else
-			if number < FACTOR[9] {
-				(number, 8)
+			if number < FACTOR[09] {
+				(number, 08)
 			} else {
 				let prefix = if number < FACTOR[13] {
 					if number < FACTOR[11] {
-						if number < FACTOR[10] { 9 } else { 10 }
+						if number < FACTOR[10] { 09 } else { 10 }
 					} else {
 						if number < FACTOR[12] { 11 } else { 12 }
 					}
@@ -331,7 +417,9 @@ impl TryFrom<f64> for Signifix {
 				(number / FACTOR[prefix], prefix)
 			}
 		};
-		let scaled = |pow: f64| { (significand * pow).round() };
+		let scaled = |pow: f64| {
+			(numerator * pow).round()
+		};
 		let signed = |abs: f64| {
 			if number.is_sign_negative() { -abs } else { abs }
 		};
@@ -343,33 +431,33 @@ impl TryFrom<f64> for Signifix {
 					Err(Error::OutOfLowerBound(number))
 				} else {
 					Ok(Signifix {
-						prefix,
-						precision: 3,
-						significand: signed(lower / 1e+03),
+						numerator: signed(lower) as i16,
+						exponent: 3,
+						prefix: prefix as u8,
 					})
 				}
 			} else {
 				Ok(Signifix {
-					prefix,
-					precision: 2,
-					significand: signed(middle / 1e+02),
+					numerator: signed(middle) as i16,
+					exponent: 2,
+					prefix: prefix as u8,
 				})
 			}
 		} else {
 			let upper = scaled(1e+01);
 			if upper < 1e+04 {
 				Ok(Signifix {
-					prefix,
-					precision: 1,
-					significand: signed(upper / 1e+01),
+					numerator: signed(upper) as i16,
+					exponent: 1,
+					prefix: prefix as u8,
 				})
 			} else {
 				let prefix = prefix + 1;
 				if prefix < FACTOR.len() {
 					Ok(Signifix {
-						prefix,
-						precision: 3,
-						significand: signed(1e+00),
+						numerator: signed(1e+03) as i16,
+						exponent: 3,
+						prefix: prefix as u8,
 					})
 				} else {
 					if number.is_nan() {
@@ -383,18 +471,18 @@ impl TryFrom<f64> for Signifix {
 	}
 }
 
-impl std::fmt::Display for Signifix {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl Display for Signifix {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
 		if f.sign_plus() {
-			write!(f, "{:+.*} {}", self.precision,
-				self.significand, SYMBOL[self.prefix])
+			write!(f, "{:+.*} {}",
+				self.exponent(), self.significand(), self.symbol())
 		} else
-		if f.alternate() && !self.significand.is_sign_negative() {
-			write!(f, " {:.*} {}", self.precision,
-				self.significand, SYMBOL[self.prefix])
+		if f.alternate() && self.numerator().is_positive() {
+			write!(f, " {:.*} {}",
+				self.exponent(), self.significand(), self.symbol())
 		} else {
-			write!(f, "{:.*} {}", self.precision,
-				self.significand, SYMBOL[self.prefix])
+			write!(f, "{:.*} {}",
+				self.exponent(), self.significand(), self.symbol())
 		}
 	}
 }
@@ -402,85 +490,95 @@ impl std::fmt::Display for Signifix {
 #[cfg(test)]
 mod tests {
 	use super::*;
-	fn fmt_def(number: f64) -> Result<String> {
+	use std::f64;
+	use std::mem::size_of;
+
+	fn def(number: f64) -> Result<String> {
 		Ok(format!("{}", Signifix::try_from(number)?))
 	}
-	fn fmt_pos(number: f64) -> Result<String> {
+	fn pos(number: f64) -> Result<String> {
 		Ok(format!("{:+}", Signifix::try_from(number)?))
 	}
-	fn fmt_alt(number: f64) -> Result<String> {
+	fn alt(number: f64) -> Result<String> {
 		Ok(format!("{:#}", Signifix::try_from(number)?))
 	}
+
 	#[test]
 	fn metric_unit_prefix() {
-		assert_eq!(fmt_def(1e-24).ok(), Some("1.000 y".into()));
-		assert_eq!(fmt_def(1e-21).ok(), Some("1.000 z".into()));
-		assert_eq!(fmt_def(1e-18).ok(), Some("1.000 a".into()));
-		assert_eq!(fmt_def(1e-15).ok(), Some("1.000 f".into()));
-		assert_eq!(fmt_def(1e-12).ok(), Some("1.000 p".into()));
-		assert_eq!(fmt_def(1e-09).ok(), Some("1.000 n".into()));
-		assert_eq!(fmt_def(1e-06).ok(), Some("1.000 µ".into()));
-		assert_eq!(fmt_def(1e-03).ok(), Some("1.000 m".into()));
-		assert_eq!(fmt_def(1e+00).ok(), Some("1.000  ".into()));
-		assert_eq!(fmt_def(1e+03).ok(), Some("1.000 k".into()));
-		assert_eq!(fmt_def(1e+06).ok(), Some("1.000 M".into()));
-		assert_eq!(fmt_def(1e+09).ok(), Some("1.000 G".into()));
-		assert_eq!(fmt_def(1e+12).ok(), Some("1.000 T".into()));
-		assert_eq!(fmt_def(1e+15).ok(), Some("1.000 P".into()));
-		assert_eq!(fmt_def(1e+18).ok(), Some("1.000 E".into()));
-		assert_eq!(fmt_def(1e+21).ok(), Some("1.000 Z".into()));
-		assert_eq!(fmt_def(1e+24).ok(), Some("1.000 Y".into()));
+		assert_eq!(def(1e-24).ok(), Some("1.000 y".into()));
+		assert_eq!(def(1e-21).ok(), Some("1.000 z".into()));
+		assert_eq!(def(1e-18).ok(), Some("1.000 a".into()));
+		assert_eq!(def(1e-15).ok(), Some("1.000 f".into()));
+		assert_eq!(def(1e-12).ok(), Some("1.000 p".into()));
+		assert_eq!(def(1e-09).ok(), Some("1.000 n".into()));
+		assert_eq!(def(1e-06).ok(), Some("1.000 µ".into()));
+		assert_eq!(def(1e-03).ok(), Some("1.000 m".into()));
+		assert_eq!(def(1e+00).ok(), Some("1.000  ".into()));
+		assert_eq!(def(1e+03).ok(), Some("1.000 k".into()));
+		assert_eq!(def(1e+06).ok(), Some("1.000 M".into()));
+		assert_eq!(def(1e+09).ok(), Some("1.000 G".into()));
+		assert_eq!(def(1e+12).ok(), Some("1.000 T".into()));
+		assert_eq!(def(1e+15).ok(), Some("1.000 P".into()));
+		assert_eq!(def(1e+18).ok(), Some("1.000 E".into()));
+		assert_eq!(def(1e+21).ok(), Some("1.000 Z".into()));
+		assert_eq!(def(1e+24).ok(), Some("1.000 Y".into()));
 	}
 	#[test]
 	fn fixed_significance() {
-		assert_eq!(fmt_def(1.234e+02).ok(), Some("123.4  ".into()));
-		assert_eq!(fmt_def(1.234e+03).ok(), Some("1.234 k".into()));
-		assert_eq!(fmt_def(1.234e+04).ok(), Some("12.34 k".into()));
-		assert_eq!(fmt_def(1.234e+05).ok(), Some("123.4 k".into()));
-		assert_eq!(fmt_def(1.234e+06).ok(), Some("1.234 M".into()));
+		assert_eq!(def(1.234e+02).ok(), Some("123.4  ".into()));
+		assert_eq!(def(1.234e+03).ok(), Some("1.234 k".into()));
+		assert_eq!(def(1.234e+04).ok(), Some("12.34 k".into()));
+		assert_eq!(def(1.234e+05).ok(), Some("123.4 k".into()));
+		assert_eq!(def(1.234e+06).ok(), Some("1.234 M".into()));
 	}
 	#[test]
 	fn formatting_options() {
-		assert_eq!(fmt_def(-1e+00).ok(), Some("-1.000  ".into()));
-		assert_eq!(fmt_def( 1e+00).ok(), Some( "1.000  ".into()));
-		assert_eq!(fmt_pos(-1e+00).ok(), Some("-1.000  ".into()));
-		assert_eq!(fmt_pos( 1e+00).ok(), Some("+1.000  ".into()));
-		assert_eq!(fmt_alt(-1e+00).ok(), Some("-1.000  ".into()));
-		assert_eq!(fmt_alt( 1e+00).ok(), Some(" 1.000  ".into()));
+		assert_eq!(def(-1e+00).ok(), Some("-1.000  ".into()));
+		assert_eq!(def( 1e+00).ok(), Some( "1.000  ".into()));
+		assert_eq!(pos(-1e+00).ok(), Some("-1.000  ".into()));
+		assert_eq!(pos( 1e+00).ok(), Some("+1.000  ".into()));
+		assert_eq!(alt(-1e+00).ok(), Some("-1.000  ".into()));
+		assert_eq!(alt( 1e+00).ok(), Some(" 1.000  ".into()));
 	}
 	#[test]
 	fn lower_prefix_bound() {
-		assert_eq!(fmt_def(-0.99951e-24).ok(), Some("-1.000 y".into()));
-		assert_eq!(fmt_def(-0.99949e-24).err(),
+		assert_eq!(def(-0.99951e-24).ok(), Some("-1.000 y".into()));
+		assert_eq!(def(-0.99949e-24).err(),
 			Some(Error::OutOfLowerBound(-0.99949e-24)));
 	}
 	#[test]
 	fn upper_prefix_bound() {
-		assert_eq!(fmt_def(-0.999949e+27).ok(), Some("-999.9 Y".into()));
-		assert_eq!(fmt_def(-0.999951e+27).err(),
+		assert_eq!(def(-0.999949e+27).ok(), Some("-999.9 Y".into()));
+		assert_eq!(def(-0.999951e+27).err(),
 			Some(Error::OutOfUpperBound(-0.999951e+27)));
 	}
 	#[test]
 	fn upper_prefix_round() {
-		assert_eq!(fmt_def(0.999949e+06).ok(), Some("999.9 k".into()));
-		assert_eq!(fmt_def(0.999951e+06).ok(), Some("1.000 M".into()));
+		assert_eq!(def(0.999949e+06).ok(), Some("999.9 k".into()));
+		assert_eq!(def(0.999951e+06).ok(), Some("1.000 M".into()));
 	}
 	#[test]
 	fn fp_category_safety() {
-		assert_eq!(fmt_def(0.0).err(),
+		assert_eq!(def(0.0).err(),
 			Some(Error::OutOfLowerBound(0.0)));
-		assert_eq!(fmt_def(std::f64::NEG_INFINITY).err(),
-			Some(Error::OutOfUpperBound(std::f64::NEG_INFINITY)));
-		assert_eq!(fmt_def(std::f64::INFINITY).err(),
-			Some(Error::OutOfUpperBound(std::f64::INFINITY)));
-		assert_eq!(fmt_def(std::f64::NAN).err(),
+		assert_eq!(def(f64::NEG_INFINITY).err(),
+			Some(Error::OutOfUpperBound(f64::NEG_INFINITY)));
+		assert_eq!(def(f64::INFINITY).err(),
+			Some(Error::OutOfUpperBound(f64::INFINITY)));
+		assert_eq!(def(f64::NAN).err(),
 			Some(Error::Nan));
 	}
 	#[test]
 	fn ord_implementation() {
-		assert!(Signifix::try_from(1e+03).unwrap()
-			< Signifix::try_from(1e+06).unwrap());
-		assert!(Signifix::try_from(1e+03).unwrap()
-			< Signifix::try_from(2e+03).unwrap());
+		assert!(Signifix::try_from(1e+03).ok()
+			< Signifix::try_from(1e+06).ok());
+		assert!(Signifix::try_from(1e+01).ok()
+			< Signifix::try_from(1e+02).ok());
+		assert!(Signifix::try_from(1e+03).ok()
+			< Signifix::try_from(2e+03).ok());
+	}
+	#[test]
+	fn mem_size_of_struct() {
+		assert_eq!(size_of::<Signifix>(), 4);
 	}
 }
